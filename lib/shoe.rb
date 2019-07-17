6a12c1692b94441aa97f7d94146dc299b99b45ae
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-17 16:18:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition  # remove the attr_accessor for genre
+  attr_reader :title, :genre  # add an attr_reader for genre
+ 
+  GENRES = []
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def cobble
+    puts "The show has been repaired!"
+  end
+ 
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
+end# Make your shoe class here!
